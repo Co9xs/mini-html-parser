cf2597613e3bab3bf87d9ca85decd8789f4ad560
Co9xs <ryo.fjsm0203@icloud.com>
2021-06-09 22:12:51 +0900

    change input html string

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import { Lexer } from './lexer/lexer'
 import { TokenTypes } from './token/token'
 
-const input = `<!DOCTYPE lang="en"><html>test</html>`
+const input = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+</body>
+</html>
+`
 const lexer = new Lexer(input)
 
 for (let token = lexer.nextToken(); token.Type !== TokenTypes.EOF; token = lexer.nextToken()) {
